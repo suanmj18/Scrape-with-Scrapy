d0cce98aa26281d04880a34008b0c42e1e157455
sumit.intrigd <sumit.intrigd@gmail.com>
2021-06-12 23:41:54 +0530

    extra file removed

--- crawl.py
+++
@@ -1,8 +0,0 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-
-process = CrawlerProcess(get_project_settings())
-
-process.crawl('insider_html')
-process.crawl('i_list')
-process.crawl('naad')
